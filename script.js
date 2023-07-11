71d2393429692ce9e887d64c022b64b44945bae3
iwillbemicrosoftceo <iwillbemicrosoftceo@gmail.com>
2023-07-11 15:57:11 -0400

    Added my project

---
+++ script.js
@@ -0,0 +1,34 @@
+const token = "pk.eyJ1IjoiaG9sb2thNTY1NiIsImEiOiJjbGp5aTNsc2owNHJxM21wNGJpczR5dnFpIn0.5PTg-ZppRDvwgZq4z4i5qg"
+mapboxgl.accessToken = token
+
+// Location Grabbing
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, { enableHighAccuracy: true })
+
+// Location Functions
+function successLocation(position) { 
+    //console.log(position)
+    setupMap([position.coords.longitude, position.coords.latitude])
+}
+
+function errorLocation() { 
+    console.error("Oh no! We could not determine where you are so you are in London!")
+    alert("Oh no! We could not determine where you are so you are in London!")
+    setupMap([-0.127758, 51.507351])
+}
+
+// Map Creating
+function setupMap(c) { 
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v12',
+        center: c,
+        zoom: 15
+    });
+    map.addControl(
+        new MapboxDirections({
+        accessToken: mapboxgl.accessToken
+        }),
+        'top-left'
+        );
+    map.addControl(new mapboxgl.NavigationControl());
+}
